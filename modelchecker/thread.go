23b0ab9fc5f42af325358e504a4e15ffaea1470b
Jayaprabhakar Kadarkarai <jp@fizzbee.io>
2023-12-15 15:06:37 -0800

    protopath

--- modelchecker/thread.go
+++ modelchecker/thread.go
@@ -1,92 +1,117 @@
 package modelchecker
 
 import (
-	"github.com/jayaprabhakar/fizzbee/lib"
-	"go.starlark.net/starlark"
+    "fizz/ast"
+    "github.com/jayaprabhakar/fizzbee/lib"
+    "go.starlark.net/starlark"
 )
 
 type Heap struct {
-	globals starlark.StringDict
+    globals starlark.StringDict
 }
 
 func (h *Heap) update(k string, v starlark.Value) bool {
-	if _, ok := h.globals[k]; ok {
-		h.globals[k] = v
-		return true
-	}
-	return false
+    if _, ok := h.globals[k]; ok {
+        h.globals[k] = v
+        return true
+    }
+    return false
 }
 
 func (h *Heap) Clone() *Heap {
-	return &Heap{CloneDict(h.globals)}
+    return &Heap{CloneDict(h.globals)}
 }
 
 type Scope struct {
-	// parent is the parent scope, or nil if this is the global scope.
-	parent *Scope
-	// vars is the set of variables defined in this scope.
-	vars starlark.StringDict
+    // parent is the parent scope, or nil if this is the global scope.
+    parent *Scope
+    // vars is the set of variables defined in this scope.
+    vars starlark.StringDict
 }
 
 func (s *Scope) Lookup(name string) (starlark.Value, bool) {
-	v, ok := s.vars[name]
-	if !ok && s.parent != nil {
-		return s.parent.Lookup(name)
-	}
-	return v, ok
+    v, ok := s.vars[name]
+    if !ok && s.parent != nil {
+        return s.parent.Lookup(name)
+    }
+    return v, ok
 }
 
 // GetAllVisibleVariables returns all variables visible in this scope.
 func (s *Scope) GetAllVisibleVariables() starlark.StringDict {
-	dict := starlark.StringDict{}
-	s.getAllVisibleVariablesToDict(dict)
-	return dict
+    dict := starlark.StringDict{}
+    s.getAllVisibleVariablesToDict(dict)
+    return dict
 }
 
 func (s *Scope) getAllVisibleVariablesToDict(dict starlark.StringDict) {
-	if s.parent != nil {
-		s.parent.getAllVisibleVariablesToDict(dict)
-	}
-	CopyDict(s.vars, dict)
+    if s.parent != nil {
+        s.parent.getAllVisibleVariablesToDict(dict)
+    }
+    CopyDict(s.vars, dict)
 }
 
 func CloneDict(oldDict starlark.StringDict) starlark.StringDict {
-	return CopyDict(oldDict, nil)
+    return CopyDict(oldDict, nil)
 }
 
 // CopyDict copies values `from` to `to` overriding existing values. If the `to` is nil, creates a new dict.
 func CopyDict(from starlark.StringDict, to starlark.StringDict) starlark.StringDict {
-	if to == nil {
-		to = make(starlark.StringDict)
-	}
-	for k, v := range from {
-		to[k] = v
-	}
-	return to
+    if to == nil {
+        to = make(starlark.StringDict)
+    }
+    for k, v := range from {
+        to[k] = v
+    }
+    return to
 }
 
 type CallFrame struct {
-	// pc is the program counter, pointing at the next instruction to execute.
-	pc int
-	// scope is the lexical scope of the current frame
-	scope *Scope
+    // File is the ast.File that this frame is executing.
+    File *ast.File
+    // pc is the program counter, pointing at the next instruction to execute.
+    pc int
+    // scope is the lexical scope of the current frame
+    scope *Scope
 }
 
 // Thread represents a thread of execution.
 type Thread struct {
-	Stack *lib.Stack[*CallFrame]
+    Stack *lib.Stack[*CallFrame]
 }
 
 func NewThread() *Thread {
-	return &Thread{lib.NewStack[*CallFrame]()}
+    return &Thread{lib.NewStack[*CallFrame]()}
 }
 
 func (t *Thread) currentFrame() *CallFrame {
-	frame, err := t.Stack.Peek()
-	panic(err)
-	return frame
+    frame, err := t.Stack.Peek()
+    panic(err)
+    return frame
 }
 
 func (t *Thread) pushFrame(frame *CallFrame) {
-	t.Stack.Push(frame)
+    t.Stack.Push(frame)
+}
+
+func (t *Thread) popFrame() *CallFrame {
+    frame, err := t.Stack.Pop()
+    panic(err)
+    return frame
+}
+
+func (t *Thread) Clone() *Thread {
+    return &Thread{t.Stack.Clone()}
+}
+
+func (t *Thread) ExecuteAction() {
+
+}
+
+func (t *Thread) ExecuteBlock() {
+
+}
+
+func (t *Thread) FindNextStatement() {
+
 }
